7b21d0cdc0bb40b021f78f1fdbbdb258fc1ac040
Toshiyuki HIRANO <hiracchi@users.noreply.github.com>
2016-02-06 09:37:52 +0900

    Fix: roles

--- exec_provisioning.sh
+++ exec_provisioning.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
+
+export LANG=C
+export LC_ALL=C
+
 # VERBOSE="-vvvv"
 GROUP="all"
-# GROUP="apt-mirror-server"
 # USE_SSH_PASSWORD="-k -c paramiko"
 # USE_SSH_PASSWORD="-k"
 # USE_SSH_PRIVATEKEY="--private-key=~/.ssh/key.pem"
+ASK_SUDO_PASS="--ask-become-pass"
 
 # setup ssh-keys
-ansible-playbook ${VERBOSE} \
-                 -i hosts.setup-sshkeys \
-                 -c paramiko \
-                 --ask-pass \
-                 --ask-become-pass \
-                 -l ${GROUP} \
-                 setup-sshkeys.yaml \
-                 $*
+#ansible-playbook ${VERBOSE} \
+#                 -i hosts.setup-sshkeys \
+#                 -c paramiko \
+#                 --ask-pass \
+#                 --ask-become-pass \
+#                 -l ${GROUP} \
+#                 setup-sshkeys.yaml \
+#                 $*
 
 # main
 ansible-playbook ${VERBOSE} \
-                 -i hosts \
+                 ${ASK_SUDO_PASS} \
+                 -i th-j1800n.hosts \
                  -l ${GROUP} \
-                 ubuntu-basics.yaml \
+                 th-j1800n.yaml \
                  $*
 
