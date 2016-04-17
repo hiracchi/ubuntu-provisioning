a0318c928927f704b5d1c6c43cdfe5b42fffbbac
Toshiyuki HIRANO <hiracchi@users.noreply.github.com>
2016-04-17 13:10:48 +0900

    Fix: update installing ansible

--- install_ansible.sh
+++ install_ansible.sh
@@ -8,11 +8,18 @@ install_ansible_system()
     sudo apt-get install ansible
 }
 
-install_ansible_current()
+install_pip()
 {
-    pip install --upgrade --user git+https://github.com/ansible/ansible.git
-    pip install --upgrade --user passlib
+    wget -o get-pip.py "https://bootstrap.pypa.io/get-pip.py"
+    python get-pip.py --user
 }
 
 install_ansible_current()
+{
+    ~/.local/bin/pip install --upgrade --user git+https://github.com/ansible/ansible.git
+    ~/.local/bin/pip pip install --upgrade --user passlib
+}
+
+# install_pip
+install_ansible_current
 
